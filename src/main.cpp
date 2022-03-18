070b40d5834ba3b8af73083c85d63a2e9bbec3ae
Amos Brocco <amos.brocco@gmail.com>
2022-03-18 11:11:52 +0100

    18.3.2022 - Rule of three

--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,105 @@
 #include <iostream>
 #include <string>
-#include "functions.h"
 
-// void say(); // forward declaration
-void say(int x=0); // Default values go into the declaration, not the definition
+struct Owner {
+    Owner(const std::string& n) : m_name{n}{
+        std::cout << "Constructing Owner " << n << '\n';
+    }
+    /*~Owner() {
+        std::cout << "Destroying Owner " << m_name << '\n';
+    }*/
+    std::string m_name; // we assume that std::string is able to copy itself
+};
 
-namespace bar { // namespace declaration
-    void foo() {
-         std::cout << "This is bar::foo()" << std::endl;
+/*struct Owner {
+    Owner(const std::string& n) : m_name{new std::string{n}}{
+        std::cout << "Constructing Owner " << n << '\n';
+    }
+    ~Owner() {
+        std::cout << "Destroying Owner " << m_name << '\n';
+        delete m_name;
     }
 
-    void baz() {
-        std::cout << "This is bar::baz()" << std::endl;
+    Owner(const Owner& o) : m_name{new std::string{*o.m_name}} {}
+
+    Owner& operator=(const Owner& o) {
+        *m_name = *o.m_name;
+        return *this;
     }
-}
 
-// hierarchical namespace declarations are also possible
-namespace supsi {
-    namespace bla {
-        int add(int x, int y) {
-            return 0;
-        }
+    std::string *m_name; // we assume that std::string is able to copy itself
+};*/
+
+struct Engine {
+    Engine() {
+        std::cout << "Constructing Engine\n";
     }
+    ~Engine() {
+        std::cout << "Destroying Engine\n";
+    }
+    int m_year{2022};
+};
+
+struct Car {
+    Car(const std::string& name,
+        const std::string& owner_name) 
+        : m_owner{new Owner{owner_name}}
+    {
+        std::cout << "Constructing Car " << name << '\n';
+        ++m_instances;
+    }
+
+    // Copy constructor
+    Car(const Car& c)
+        : m_owner{new Owner{*c.m_owner}} { // we assume that Owner is able to copy itself
+        std::cout << "Copy constructing Car " << '\n';
+        ++m_instances;
+    }
+
+    // Copy assignment operator
+    Car& operator=(const Car& c) {
+        *m_owner = *c.m_owner; // we assume that Owner is able to copy itself
+        return *this;
+    }
+
+    ~Car() {
+        std::cout << "Destroying Car " << '\n';
+        delete m_owner;
+        --m_instances;
+    }
+
+    static int how_many_cars() {
+        return m_instances;
+    }
+
+private:
+    static int m_instances;
+    Engine m_a;
+    Owner *m_owner;
+};
+
+int Car::m_instances = 0; // static field initialization
+
+// Construction of a copy
+void foo(Car c) {
+    std::cout << "I'm in foo\n";
+    std::cout << "How many cars? " << Car::how_many_cars() << "\n";
 }
 
-void foo() {
-    say();
+// Construction of a copy
+void foo2(const Car &c) {
+    std::cout << "I'm in foo\n";
+    std::cout << "How many cars? " << Car::how_many_cars() << "\n";
 }
-
-// definition / implementation
-/*void say() {
-    //std::cout << "This is say(0)" << std::endl;
-    say(0);
-}*/
-
-void say(int x) {
-    std::cout << "This is say(" << x << ")" << std::endl;
-}
-
-int mul(int x = 0, int y = 1, int z = 1) {
-    return x*y*z;
-}
-
-// Will be ambiguous if we call mul()
-/*int mul() {
-    return 0;
-}*/
-
-void myfun(int x, double y) {
-}
-
-void myfun(double x, int y) {
-}
-
-auto somefun() {
-    return 3;
-}
-
-void fromjava() {
-    //auto* mystr = new std::string{"Course"}; // mystr is allocated on the HEAP
-    std::string mystr{"Course"}; // mystr is on the STACK
-    //std::string s = new std::string{"blabla"}; // Not correct: tries to assign a pointer to a value
-    std::string t; // t is a string value!
-    std::cout << t << '\n';
-    //delete mystr; // WE HAVE TO FREE THE MEMORY (no GC)
-}
-
-namespace fn = supsi::bla; // Create an alias for supsi::bla -> we can use fn instead of supsi::bla
-using std::endl; // Import one 'symbol' in the global namespace
-// using namespace bar; // Import all the namespace 'bar' inside the global namespace ---> BAD PRACTICE
 
 int main()
 {
-    std::cout << "Hello world" << endl;
-    std::cout << bar::add(5, 6) << endl;
-    std::cout << supsi::bla::add(5, 6) << endl;
-    std::cout << fn::add(5,6) << endl;
-    say();
-    say(5);
-    mul(3,6);
-    mul(3,4,5);
-    mul(3);
-    //mul();
-    bar::sub(7);
-    // bar::foo(); // Ambiguous: there are multiple foo in the bar namespace
-    myfun(3.4, 4);
-    myfun(4,3.14);
-    // myfun(2,3); // Ambiguous: either myfun(double,int) or myfun(int,double) match this call
-    myfun(2,double(3)); // or myfun(2,3.0);
+    Car b{"Supsi", "Amos"};
+    foo(b);
+    Car copy{b}; // Copy constructor
+    std::cout << "How many cars? " << Car::how_many_cars() << "\n";
 
-
-    std::string message = "Héllo world";
-    std::cout << message << " - " << message.length() << '\n';
-    message += "ciao";
-    std::string a = "A";
-    std::string b = "B";
-    if (a == b) {
-
-    } else if (a == "C") {
-
-    }
-    std::cout << "First:" << message[0] << '\n';
-
-    int x = 0; // C-style declaration/initialization
-    int y(8); // C++ (old-style)
-    int z{3}; // Preferred way in C++11
-    int k = {7}; // ...
-
-    int d = 3.14;
-    // int s{3.14}; // Error!
-    std::string salute{"Hello moon"};
-
-    auto pi{3.14};
-    auto result{mul(1,2,3)};
-
-    auto mystr{"Hello mars"}; // mystr is of type const char*
-    std::string mystring{"Hello mars"};
-
-    int myarr[] {1,2,3,4};
-    for (int x : myarr) {
-        std::cout << x << '\n';
-    }
-    for (auto y : myarr) {
-        std::cout <<y << '\n';
-    }
-    for (auto z : {9,8,7,6,5,4,3,2,1}) {
-        std::cout << z << '\n';
-    }
-
-    {
-        auto* p = new int{42};
-        int* q = nullptr; 
-        if (!q) {
-
-        }
-        delete p;
-    }
-
+    Car d{"Usi", "Luca"};
+    b = d; // Assignment of a copy, like b.operator=(d)
+    std::cout << "How many cars? " << Car::how_many_cars() << "\n";
 }
