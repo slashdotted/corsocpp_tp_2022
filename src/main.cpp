cd9e0075217c5bd19c03496c2dd6d1fad72ad3fb
Amos Brocco <amos.brocco@gmail.com>
2022-04-08 11:45:57 +0200

    8.4 - Casts

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,54 @@ int main() {
     using std::cout;
     LecturerResearcher john{"John", "DTI", 456, "Blabla", "FooFoo", "Coffee", 0.6};
 
-    // cout << "Name: " << john.Researcher::name();
-    cout << "Name: " << john.name();
-    cout  << " Research area: " <<john.researcharea()
-        << " %:"<< john.researchpercentage() << " Course:"<< john.course() << '\n';
+    // static_cast<DestType>(source)
+    // =============================
+{
+    // You can static_cast values
+    Employee e = static_cast<Employee>(john);
 
-    lecturerresearcher_addr(&john);
-    lecturer_addr(&john); 
-    researcher_addr(&john); 
-    employee_addr(&john); 
+    // You can static_cast refs or pointers
+    auto re = static_cast<Researcher&>(john);
+    auto le = static_cast<Lecturer*>(&john);
+
+    auto ee = static_cast<Employee*>(&john);
+    // Lecturer* lee = static_cast<Lecturer*>(ee);
+
+    Lecturer alice{"Alice", "DTI", 535, "Foo", "Bar"};
+    LecturerResearcher* alicelr = static_cast<LecturerResearcher*>(&alice);
+    std::cout << alicelr->researchpercentage() << '\n';
 }
+    // dynamic_cast<DestType>(source)
+    // =============================
+{   
+    // You cannot dynamic_cast values 
+    // Employee e = dynamic_cast<Employee>(john);
+
+    // Upcasting works as expected
+
+    // You can dynamic_cast refs or pointers
+    auto re = dynamic_cast<Researcher&>(john);
+    auto le = dynamic_cast<Lecturer*>(&john);
+    auto ee = dynamic_cast<Employee*>(&john);
+
+
+    Lecturer alice{"Alice", "DTI", 535, "Foo", "Bar"};
+    LecturerResearcher* alicelr = dynamic_cast<LecturerResearcher*>(&alice);
+    if (alicelr) {
+        std::cout << alicelr << '\n';
+    }
+
+    try {
+        LecturerResearcher& alicelr2 = dynamic_cast<LecturerResearcher&>(alice);
+    } catch(...) {
+
+    }
+    
+    //void* aliceptr = &alice;
+    //LecturerResearcher* aliceptr2 = dynamic_cast<LecturerResearcher*>(aliceptr);
+    const LecturerResearcher* calicelr = dynamic_cast<LecturerResearcher*>(&alice);
+    LecturerResearcher* ncalicelr = const_cast<LecturerResearcher*>(calicelr);
+}
+
+}
+
